27158dfcf6f7581a0f0de378c20d48bb1122006d
Jyotsna Jose <jyotsnamaria@gmail.com>
2021-03-22 18:37:28 -0500

    added rel on footer and updated href in Current weather component

--- src/Footer.js
+++ src/Footer.js
@@ -7,6 +7,7 @@ export default function Footer() {
       <a
         href="https://github.com/JyotsnaJose/react-weather-app"
         target="_blank"
+        rel="noreferrer"
       >
         {" "}
         Open-source code
